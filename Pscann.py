d96f639a06dd072f6268ada9d30f42fbb2cd9511
shaan453 <75296055+shaan453@users.noreply.github.com>
2020-12-08 05:57:53 -0800

    Add files via upload

--- Pscann.py
+++ Pscann.py
@@ -5,14 +5,33 @@ import threading
 import pyfiglet
 bS = pyfiglet.figlet_format("Pscann")
 print(bS)
+print("				Code by: Shaan453")
+fileName = ""
+lineOfList = "Port scan report \n "
 def scanPorts(port):
 	con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	final = con.connect_ex((target,port))
 	if final:
 		pass
 	else:
-		print("Port {} is open".format(port))
+		portSet = "Port {} is open".format(port)
+		print(portSet)
+		if sys.argv[-2] == "-o":
+			file = open(sys.argv[-1]+".txt","a")
+			file.write(portSet+"\n")
+			file.close()
+		else:
+			pass
 	con.close()
+
+def saveFile(fileName):
+	try:
+		save = open(fileName,"a")
+	except:
+		print("File not found")
+	save.write(lineOfList)
+	save.close()
+
 usage = "Usage: Pscann.py <IP-ADDRESS> <STARTING-PORT> <ENDING-PORT>"
 target = socket.gethostbyname(sys.argv[1])
 startingPort = 0
@@ -23,12 +42,15 @@ if sys.argv[2] == "-sH":
 	endingPort = 500
 if sys.argv[2] == "-sB":
 	endingPort = 100
+else:
+	pass
+
 if sys.argv[2] == "--help":
 	fileHelp = open("help.txt","r")
 	print(fileHelp.read())
-if len(sys.argv) != 3:
-    print(usage)
 else:
-	for port in range(startingPort,endingPort+1):
-		Thread = threading.Thread(target = scanPorts,args=(port,))
-		Thread.start();
+	pass
+
+for port in range(startingPort,endingPort+1):
+	Thread = threading.Thread(target = scanPorts,args=(port,))
+	Thread.start()
