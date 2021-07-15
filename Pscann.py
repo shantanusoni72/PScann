0e09745f83429b841465f62d742faadb5995bdd3
shaan453 <75296055+shaan453@users.noreply.github.com>
2021-07-15 14:26:16 +0530

    Add files via upload

--- Pscann.py
+++ Pscann.py
@@ -2,57 +2,18 @@ import socket
 import sys
 import time
 import threading
-import pyfiglet
-def banner():
-	print(pyfiglet.figlet_format("Pscann"))
-	print("				Code by: Shaan453")
-	print("                                Version: 2021.3.2")
-banner()
-fileName = ""
-lineOfList = "Port scan report \n "
-def scanning():
-	for port in range(startingPort,endingPort+1):
-		Thread = threading.Thread(target = scanPorts,args=(port,))
-		Thread.start()
-def scanPorts(port):
-	con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	final = con.connect_ex((target,port))
-	if final:
-		pass
-	else:
-		portSet = "Port {} is open".format(port)
-		print(portSet)
-		if sys.argv[-2] == "-o":
-			file = open(sys.argv[-1]+".txt","a")
-			file.write(portSet+"\n")
-			file.close()
-		else:
-			pass
-	con.close()
+from build.scanning import *
+import os
 
-def saveFile(fileName):
-	try:
-		save = open(fileName,"a")
-	except:
-		print("File not found")
-	save.write(lineOfList)
-	save.close()
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
-usage = "Usage: Pscann.py <IP-ADDRESS> <STARTING-PORT> <ENDING-PORT>"
-if len(sys.argv) == 1 or sys.argv[1] == "--help":
+usage = "Usage: Pscann.py <IP-ADDRESS> <option(s)>"
+if len(sys.argv) == 1 or sys.argv[1] == "--help" or len(sys.argv) == 2:
 	fileHelp = open("help.txt","r")
 	print(fileHelp.read())
+	sys.exit()
 else:
 	target = socket.gethostbyname(sys.argv[1])
-	startingPort = 0
-	endingPort = 0
-	if len(sys.argv) == 2 or len(sys.argv) == 1:
-		print(usage)
-		sys.exit()
-	if sys.argv[2] == "-sF":
-		endingPort = 1000
-	if sys.argv[2] == "-sH":
-		endingPort = 500
-	if sys.argv[2] == "-sB":
-		endingPort = 100
-	scanning()
+	option = sys.argv[2]
+	print(banner())
+	mode(target, option)
---
+++ build/scanning.py
@@ -0,0 +1,52 @@
+import sys
+import socket
+import threading
+import datetime
+
+def banner():
+	return '-------------------------------------Pcann-2021.5.3-Scan-----------\n'+'-'*41+str(datetime.datetime.now())
+
+def scanning(target, startingPort, endingPort):
+    for port in range(startingPort,endingPort+1):
+        Thread = threading.Thread(target = scanPorts,args=(target,port,)) 
+        Thread.start()
+
+def scanPorts(target,port):
+    con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    final = con.connect_ex((target,port))
+    if port == 63000 or port == 30000 or port == 1000:
+        pass
+    else:
+        if final:
+            pass
+        else:
+            portSet = "Port {} is open".format(port)
+            print(portSet)
+            if sys.argv[-2] == "-o":
+                file = open(sys.argv[-1]+".txt","a")
+                file.write(portSet+"\n")
+                file.close()
+            else:
+                pass
+            con.close()
+
+def saveFile(fileName):
+    fileName = ""
+    lineOfList = "Port scan report \n"
+    try:
+        save = open(fileName,"a")
+    except:
+        print("File not found")
+    save.write(lineOfList)
+    save.close()
+
+def mode(target, option):
+    startingPort = 0
+    endingPort = 0
+    if option == "-sF":
+        endingPort = 63000
+    if option == "-sH":
+        endingPort = 30000
+    if option == "-sB":
+        endingPort = 1000
+    scanning(target, startingPort, endingPort)
